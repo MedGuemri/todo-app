3ed0fb9358c2cf4a2cb1ef26c5cdc0a9251ae40e
Mouhamed guemri <mohamedamiineguemri@gmail.com>
2023-04-25 01:17:42 +0200

    redux check point

---
+++ todolist/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ListTask from './component/ListTask';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ListTask />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
